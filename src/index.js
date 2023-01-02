ae93e03c22b98453c7b33cfb5db0b0283c286b0b
willy836 <wilsonkimiri836@gmail.com>
2023-01-02 19:19:21 +0300

    Fix eslint error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 const title = document.createElement('h3');
-title.textContent = "GROUP CAPSTONE PROJECT!";
+title.textContent = 'GROUP CAPSTONE PROJECT!';
 
 const body = document.querySelector('body');
 body.appendChild(title);
