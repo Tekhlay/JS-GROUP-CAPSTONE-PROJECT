88f2fd2d0046a8520f16cf08b418add586393b65
Teklay <teklay@gmail.com>
2023-01-03 00:22:58 +0300

    Add header and footer sections

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import './style.css';
-
-const title = document.createElement('h3');
-title.textContent = 'GROUP CAPSTONE PROJECT!';
-
-const body = document.querySelector('body');
-body.appendChild(title);
