5ff217d0fa5b633f923c19ece1161ca9f13b6456
willy836 <wilsonkimiri836@gmail.com>
2023-01-02 19:03:00 +0300

    Setup webpack and linters

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+const title = document.createElement('h3');
+title.textContent = "GROUP CAPSTONE PROJECT!";
+
+const body = document.querySelector('body');
+body.appendChild(title);
