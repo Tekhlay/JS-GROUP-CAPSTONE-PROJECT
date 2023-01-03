0aef0d08d16c6dcdfb77a05cc395fdc295455353
Teklay <teklay@gmail.com>
2023-01-03 21:52:29 +0300

    Create comment popup menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 import './style.css';
+import getcomments from './module/commentspopup.js';
+
+const linkbreakfast = document.querySelector('.link-breakfast');
+const linkpasta = document.querySelector('.link-pasta');
+const linkchicken = document.querySelector('.link-chicken');
+// const li = document.querySelectorAll('li');
+
+const breakfasturl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast';
+
+const pastaurl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=pasta';
+
+const chickenurl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken';
+
+const displayitems = (element) => {
+  const fooditem = document.querySelector('.food-container');
+  fooditem.innerHTML = '';
+  element.forEach((e) => {
+    const div = document.createElement('div');
+    div.classList.add('food-card');
+    div.id = e.idMeal;
+    div.innerHTML = `
+     <div class="food-img"> <img src="${e.strMealThumb}" alt="#"></div>
+      <h3 class="food-title">${e.strMeal}</h3>
+      <div class="reactions">
+      <button class="comments">Coments</button>
+      <p class="likes">Likes</p>
+     </div> `;
+    const comments = div.querySelector('.comments');
+    comments.addEventListener('click', () => {
+      getcomments(e.idMeal);
+    });
+    fooditem.appendChild(div);
+  });
+};
+
+const getListitems = async (url) => {
+  const request = new Request(url);
+  const response = await fetch(request);
+  const data = await response.json();
+  const data1 = data.meals;
+  // console.log(data1);
+  displayitems(data1);
+};
+
+linkbreakfast.addEventListener('click', () => {
+  linkbreakfast.style.cursor = 'pointer'
+  getListitems(breakfasturl);
+});
+
+linkpasta.addEventListener('click', () => {
+  getListitems(pastaurl);
+});
+
+linkchicken.addEventListener('click', () => {
+  getListitems(chickenurl);
+});
---
+++ src/module/commentspopup.js
@@ -0,0 +1,44 @@
+const fooditem = document.querySelector('.food-container');
+// const closePopup = (elem) => {
+//   fooditem.classList.toggle('no-grid');
+  
+// };
+
+const popup = (items) => {
+  const popupwindow = document.createElement('article');
+  popupwindow.classList.add('popup-window');
+  const btnclose = document.createElement('div');
+  btnclose.classList.add('close');
+  btnclose.innerHTML = '&times;';
+  const popdata = document.createElement('div')
+  popdata.classList.add('pop-content');
+  popdata.innerHTML = `
+ <div class="food-details">
+ <img src="${items.strMealThumb}" alt="" width="250px">
+ <h2>${items.strMeal}</h2>
+ <h4>Ingridents </h4>
+ <p>${items.strIngredient1},${items.strIngredient2},${items.strIngredient3} </p>
+ <h4>Recipe Instructions </h4>
+ <p class='recipe'> ${items.strInstructions} </p>
+ </div>
+  `;
+  btnclose.addEventListener('click', () => {
+    fooditem.removeChild(popupwindow);
+  });
+  popupwindow.appendChild(btnclose);
+  popupwindow.appendChild(popdata);
+  fooditem.appendChild(popupwindow);
+
+};
+
+const getcomments = async (id) => {
+  const url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+  const request = new Request(url + id);
+  const response = await fetch(request);
+  const result = await response.json();
+  const singleItem = result.meals[0];
+  popup(singleItem);
+  // console.log(singleItem);
+};
+
+export { getcomments as default };
