1bb7c894b28664b88e33ea04d46dde825a72c76a
willy836 <wilsonkimiri836@gmail.com>
2023-01-05 09:58:44 +0300

    Add homepage functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import getcomments from './module/commentspopup.js';
+import { addLike, itemsCounter } from './module/homepageDisplay.js';
 
 const linkbreakfast = document.querySelector('.link-breakfast');
 const linkpasta = document.querySelector('.link-pasta');
@@ -24,12 +25,16 @@ const displayitems = (element) => {
       <h3 class="food-title">${e.strMeal}</h3>
       <div class="reactions">
       <button class="comments">Coments</button>
-      <p class="likes">Likes</p>
+      <i data-id=${e.idMeal} class="fa-regular fa-heart"></i>
+      <p data-id=${e.idMeal} class="likes">Likes</p>
      </div> `;
+    const heartIcon = div.querySelector('.fa-heart');
+    heartIcon.addEventListener('click', addLike);
     const comments = div.querySelector('.comments');
     comments.addEventListener('click', () => {
       getcomments(e.idMeal);
     });
+
     fooditem.appendChild(div);
   });
 };
@@ -38,19 +43,28 @@ const getListitems = async (url) => {
   const request = new Request(url);
   const response = await fetch(request);
   const data = await response.json();
-  const data1 = data.meals;
-  // console.log(data1);
+  let  data1 = data.meals;
+  
+  linkbreakfast.innerHTML = `Breakfast (${itemsCounter(data1)})`;
+
   displayitems(data1);
+  //return data1
 };
 
-linkbreakfast.addEventListener('click', () => {
+// console.log(getListitems(breakfasturl))
+
+linkbreakfast.addEventListener('click', async () => {
   getListitems(breakfasturl);
+
+  // linkbreakfast.innerHTML = `Breakfast (${displayitems(breakfasturl)})`;
 });
 
 linkpasta.addEventListener('click', () => {
   getListitems(pastaurl);
+  // linkpasta.innerHTML = `Pasta (${itemsCounter()})`;
 });
 
 linkchicken.addEventListener('click', () => {
   getListitems(chickenurl);
+  // linkchicken.innerHTML = `Chicken (${itemsCounter()})`;
 });
